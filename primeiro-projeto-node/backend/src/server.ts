ab444c63673f82551148a7810ef6a8038dd6a293
iidutra <igordutra07@gmail.com>
2020-06-24 22:37:47 -0400

    refatorando meus projetos

--- primeiro-projeto-node/backend/src/server.ts
+++ primeiro-projeto-node/backend/src/server.ts
@@ -2,7 +2,11 @@ import express from 'express';
 import routes from './routes';
 
 const app = express();
+
 app.use(express.json());
+
 app.use(routes);
 
-export default app;
+app.listen(3333, () => {
+  console.log('Server started on port 3333');
+});
